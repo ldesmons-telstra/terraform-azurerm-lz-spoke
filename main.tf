7334686f9d0bc99e56dfca711e52a96e4c75c404
Laurent Desmons-Vichet <laurent.desmons-vichet@team.telstra.com>
2022-09-19 08:23:21 +0800

    format main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # TF main config
 terraform {
   required_version = ">= 0.12.24"
-    required_providers {
+  required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "=3.0.0"
